69c9be460368ba2df73025cb566b70835b9b4df0
Andrew Bruce <ab@jandaweb.com>
2009-07-13 09:44:03 +0100

    refactoring, new story link and began work on custom Request class

--- application.js
+++ application.js
@@ -11,8 +11,49 @@ $(document).ready( function() {
   Role.enableAll();
 
   // activate new role link
-  $('a[href="/roles/new"]').click(Role.newFromName);
+  $('a[href="/roles/new"]').click(Role.buildFromPrompt);
+
+  // activate new story link
+  $('a[href="/stories/new"]').click(Story.buildFromPrompt);
+
+  // temporarily add starter role
+  Role.build('Starter');
 });
+
+// request class
+function Request(title, options) {
+  $('#request').remove();
+
+  $('body').append(
+    '<div id="request">'+
+    '<h2>'+title+'</h2>'+
+    '<form>'+
+    '</form>'+
+    '</div>'
+  );
+
+  $(options).each( function() {
+    if (this.type == 'text') {
+      $('#request form').append(
+        '<p>'+
+        '<label for="'+this.name+'">'+this.label+'</label>'+
+        '<input id="'+this.name+'" type="text" name="'+this.name+'" value="'+this.value+'" />'+
+        '</p>'
+      );
+    }
+  });
+
+  this.params = {
+    name: ''
+  }
+}
+
+// story class
+function Story(name, content) {
+  this.name = name;
+  this.content = content;
+  Role.backlog().stories.push(this);
+}
 
 // role class
 function Role(element) {
@@ -20,27 +61,38 @@ function Role(element) {
   this.sortableElement = $(this.element).find('.stories>ol');
   this.name = $(this.element).find('h2').html(); 
 
+  // disallow creation of roles with existing names
   if ($.inArray(this.name, Role.names()) > -1) {
     Role.destroy(this.element);
     return false;
   }
 
+  // keep all new roles in Role.all
   if ($.inArray(this, Role.all) == -1) {
     Role.all.push(this);
     console.log('new role: ' + Role.all.length);
   }
 }
 Role.all = [];
-Role.newFromName = function() {
-  var name = prompt('Role name');
+Role.buildFromPrompt = function() {
+  var request = new Request('New Role', [
+    {label: 'Name', name: 'name', type: 'text', value: ''}
+  ]);
 
-  if (name) {
-    $('#roles>ol').append('<li class="role"><h2>'+name+'</h2><div class="stories><ol></ol></div></li>');
-    new Role($('#roles>ol>li:last-child'));
-    Role.enableAll();
-  }
-
+  if (request.params.name) Role.build(request.params.name);
   return false;
+}
+Role.build = function(name) {
+  $('#roles>ol').append(
+      '<li class="role">'+
+      '<h2>'+name+'</h2>'+
+      '<div class="stories active"><ol></ol></div>'+
+      '<div class="stories buffer"><ol></ol></div>'+
+      '</li>');
+  new Role($('#roles>ol>li:last-child'));
+
+  // re-enable all roles so each is draggable to/from
+  Role.enableAll();
 }
 Role.names = function() {
   return $.map(Role.all, function(role) { return role.name });
@@ -49,13 +101,17 @@ Role.destroy = function(element) {
   $(element).remove(); 
 }
 Role.enableAll = function() {
-  $(Role.all).each( function() {
+  $(Role.all).each( function() { this.enable() });
+}
+Role.receive = function(event, ui) {
+  // TODO: disallow dragging if role has reached limit
+  console.log($(this).parent().attr('class'), 'received', ui.item, 'from', ui.sender);
+}
+Role.prototype = {
+  enable: function() {
     $(this.sortableElement).sortable({
       connectWith: '.role .stories>ol',
-      receive: function(event, ui) {
-        console.log(this, 'received', ui.item, 'from', ui.sender);
-      }
-    });
-  });
+      receive: Role.receive
+    }); 
+  }
 }
-
