6a2a476751c0401c368dcbe23b0f3f7b0eb3b827
Andrew Bruce <ab@jandaweb.com>
2009-07-20 23:55:42 +0100

    Require ruby-debug (needs ruby-debug-base JRuby gem from rubyforge)

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 #require 'selenium/client'
 require 'celerity'
+require 'ruby-debug'
 
 @@browser = Celerity::Browser.new(:javascript_exceptions => true,
                                   :status_code_exceptions => true,
