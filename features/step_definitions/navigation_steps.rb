3dc1ec97de40df4ac33dbc4adedb03d4189f9466
Andrew Bruce <ab@jandaweb.com>
2009-07-20 23:54:59 +0100

    Removed commented selenium stuff

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,15 +1,12 @@
 Given /^I am on the homepage$/ do
-#  @browser.open '/heijunka/home/index.html' 
   @@browser.goto 'http://localhost:5984/heijunka/home/index.html'
 end
 
 When /^I follow '(.*)'$/ do |link_text|
   @@browser.link(:text, link_text).click
-#  @browser.click("link=#{link_text}", :wait_for => :ajax)
 end
 
 Then /^I should see '(.*)'$/ do |text|
   @@browser.contains_text(text).should_not be_nil
-#  included = @browser.text?(text)
-#  included.should be_true
 end
+
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,19 +5,4 @@ require 'celerity'
 @@browser = Celerity::Browser.new(:javascript_exceptions => true,
                                   :status_code_exceptions => true,
                                   :log_level => :severe)
-Before do
-=begin
-  @browser = Selenium::Client::Driver.new(
-    :host => 'localhost', 
-    :port => 4444,
-    :browser => '*firefox', 
-    :url => 'http://localhost:5984',
-    :javascript_framework => :jquery
-  )
-  @browser.start_new_browser_session
-=end
-end
 
-After do
-  #@browser.close_current_browser_session
-end
