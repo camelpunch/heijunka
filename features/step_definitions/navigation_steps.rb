2591955c17eb60184ad114162f02aa083cd76534
Andrew Bruce <ab@jandaweb.com>
2009-07-30 13:04:30 +0100

    Move back to Selenium from Celerity - I should really make up my mind

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,21 +1,23 @@
 Given /^I am on the homepage$/ do
-  @@browser.goto 'http://localhost:5984/heijunka/home/index.html'
+  @@browser.open 'http://localhost:5984/heijunka/home/index.html'
 end
 
 When /^I follow '(.*)'$/ do |link_text|
-  @@browser.link(:text, link_text).click
+  @@browser.click("link=#{link_text}")
 end
 
 When /^I fill in '(.*)' with '(.*)'$/ do |label, text|
+  pending
   id = @@browser.label(:text, label).for
   @@browser.text_field(:id, id).value = text
 end
 
 When /^I press '(.*)'$/ do |button_text|
+  pending
   @@browser.button(button_text).click
 end
 
 Then /^I should see '(.*)'$/ do |text|
-  @@browser.contains_text(text).should_not be_nil
+  @@browser.text?(text).should be_true
 end
 
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,20 @@
 require 'rubygems'
-require 'celerity'
+require 'selenium/client'
 require 'couchrest'
 require 'ruby-debug'
 
-@@browser = Celerity::Browser.new(:javascript_exceptions => true,
-                                  :status_code_exceptions => true,
-                                  :log_level => :severe)
 @@roles = CouchRest.database! 'roles'
 @@stories = CouchRest.database! 'stories'
 
+@@browser = Selenium::Client::Driver.new(
+    :host => 'localhost', 
+    :port => 4444,
+    :browser => '*firefox', 
+    :url => 'http://localhost:5984'
+  )
+@@browser.start_new_browser_session
+
+at_exit do
+  @@browser.close_current_browser_session
+end
 
