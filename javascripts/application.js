e9415b4691361f1a9a7602196459e5443f42f9f6
Andrew Bruce <ab@jandaweb.com>
2009-07-30 20:18:39 +0100

    load stories from backlog on start up and use new Story.create for new story link

--- javascripts/application.js
+++ javascripts/application.js
@@ -6,15 +6,25 @@ $(document).ready( function() {
     }
   });
 
+  var documents = $.jqCouch.connection('doc');
+
   // instantiate objects with HTML elements
   $('.role').each( function() { new Role(this) });
   Role.enableAll();
+
+  // build stories from backlog
+  var stories = documents.all('stories').rows;
+
+  $(stories).each( function() {
+    var story = documents.get('stories/' + this.id);
+    Story.build(story.name, story.content);
+  });
 
   // activate new role link
   $('a[href="/roles/new"]').click(Role.create);
 
   // activate new story link
-  $('a[href="/stories/new"]').click(Story.buildFromPrompt);
+  $('a[href="/stories/new"]').click(Story.create);
 
   // temporarily add starter role
   Role.build('Starter');
